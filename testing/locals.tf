bc000d45b128ef2cef109ecffa1c7b776337b45c
G Baral <ganesh.baral@hotmail.co.uk>
2023-11-09 14:41:48 +0000

    node config changed

--- testing/locals.tf
+++ testing/locals.tf
@@ -52,7 +52,7 @@ locals {
   # Other values set in main.tf (node name, asg size, ami id)
   #-------------------------------------------------------------
 
-  instance_type = "t2.medium"
+  instance_type = "t2.small"
   key_name      = "ganeshbaral-sandbox" 
   
   node_block_device = {
