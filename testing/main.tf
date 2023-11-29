2bcb5b4dfd8f9e3beb9726ec21d6fa3019fc0def
G Baral <ganesh.baral@hotmail.co.uk>
2023-11-29 13:58:05 +0000

    other add_ons changed

--- testing/main.tf
+++ testing/main.tf
@@ -121,6 +121,31 @@ resource "aws_eks_addon" "efs_csi_driver" {
   addon_version     = "v1.7.1-eksbuild.1"  # Ensure this version is compatible
   resolve_conflicts = "OVERWRITE"
 }
+
+# CNI
+resource "aws_eks_addon" "cni" {
+  cluster_name      = module.eks.cluster_id
+  addon_name        = "vpc-cni"
+  addon_version     = "v1.10.4-eksbuild.1"
+  resolve_conflicts = "OVERWRITE"
+}
+
+# coredns
+resource "aws_eks_addon" "coredns" {
+  cluster_name      = module.eks.cluster_id
+  addon_name        = "coredns"
+  addon_version     = "v1.8.7-eksbuild.2"
+  resolve_conflicts = "OVERWRITE"
+}
+
+# kube-proxy
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name      = module.eks.cluster_id
+  addon_name        = "kube-proxy"
+  addon_version     = "v1.23.7-eksbuild.1"
+  resolve_conflicts = "OVERWRITE"
+}
+
 ################################################################################
 # Notes
 ################################################################################
