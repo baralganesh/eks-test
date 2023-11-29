77ef8220ede50ffff21bd80e191c9c659dd345e8
G Baral <ganesh.baral@hotmail.co.uk>
2023-11-29 13:43:28 +0000

    version corrected

--- testing/main.tf
+++ testing/main.tf
@@ -118,7 +118,7 @@ resource "aws_eks_addon" "ebs_csi_driver" {
 resource "aws_eks_addon" "efs_csi_driver" {
   cluster_name      = module.eks.cluster_id
   addon_name        = "aws-efs-csi-driver"
-  addon_version     = "v1.7.1-eksbuild-1"  # Ensure this version is compatible
+  addon_version     = "v1.7.1-eksbuild.1"  # Ensure this version is compatible
   resolve_conflicts = "OVERWRITE"
 }
 ################################################################################
