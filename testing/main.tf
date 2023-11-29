cf772befc7c67f10d668737cc967be836b930b6c
G Baral <ganesh.baral@hotmail.co.uk>
2023-11-29 13:40:45 +0000

    efs added

--- testing/main.tf
+++ testing/main.tf
@@ -114,6 +114,13 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   resolve_conflicts = "OVERWRITE"
 }
 
+# EFS CSI Driver
+resource "aws_eks_addon" "efs_csi_driver" {
+  cluster_name      = module.eks.cluster_id
+  addon_name        = "aws-efs-csi-driver"
+  addon_version     = "v1.7.1-eksbuild-1"  # Ensure this version is compatible
+  resolve_conflicts = "OVERWRITE"
+}
 ################################################################################
 # Notes
 ################################################################################
