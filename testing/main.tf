81bcd384791d95edb75b032454e88775664858b7
G Baral <ganesh.baral@hotmail.co.uk>
2023-11-29 14:52:59 +0000

    metrics-server

--- testing/main.tf
+++ testing/main.tf
@@ -126,7 +126,7 @@ resource "aws_eks_addon" "efs_csi_driver" {
 resource "aws_eks_addon" "cni" {
   cluster_name      = module.eks.cluster_id
   addon_name        = "vpc-cni"
-  addon_version     = "v1.10.4-eksbuild.1"
+  addon_version     = "v1.10.4-eksbuild.1" # Ensure this version is compatible
   resolve_conflicts = "OVERWRITE"
 }
 
@@ -134,7 +134,7 @@ resource "aws_eks_addon" "cni" {
 resource "aws_eks_addon" "coredns" {
   cluster_name      = module.eks.cluster_id
   addon_name        = "coredns"
-  addon_version     = "v1.8.7-eksbuild.2"
+  addon_version     = "v1.8.7-eksbuild.2" # Ensure this version is compatible
   resolve_conflicts = "OVERWRITE"
 }
 
@@ -142,9 +142,75 @@ resource "aws_eks_addon" "coredns" {
 resource "aws_eks_addon" "kube_proxy" {
   cluster_name      = module.eks.cluster_id
   addon_name        = "kube-proxy"
-  addon_version     = "v1.23.7-eksbuild.1"
+  addon_version     = "v1.23.7-eksbuild.1" # Ensure this version is compatible
   resolve_conflicts = "OVERWRITE"
 }
+
+
+# Metrics-server
+resource "kubernetes_manifest" "metrics_server" {
+  manifest = {
+    apiVersion = "apps/v1"
+    kind       = "Deployment"
+    metadata = {
+      name      = "metrics-server"
+      namespace = "kube-system"
+    }
+    spec = {
+      selector = {
+        matchLabels = {
+          k8s-app = "metrics-server"
+        }
+      }
+      template = {
+        metadata = {
+          labels = {
+            k8s-app = "metrics-server"
+          }
+        }
+        spec = {
+          containers = [
+            {
+              name  = "metrics-server"
+              image = "k8s.gcr.io/metrics-server/metrics-server:v0.6.4"  # Update with the latest version
+              args = [
+                "--cert-dir=/tmp",
+                "--secure-port=4443",
+                "--kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname",
+                "--kubelet-use-node-status-port",
+                "--metric-resolution=15s"
+              ]
+              ports = [
+                {
+                  containerPort = 4443
+                  name          = "https"
+                }
+              ]
+              securityContext = {
+                readOnlyRootFilesystem = true
+                runAsNonRoot           = true
+                runAsUser              = 1000
+              }
+              volumeMounts = [
+                {
+                  mountPath = "/tmp"
+                  name      = "tmp-dir"
+                }
+              ]
+            }
+          ]
+          volumes = [
+            {
+              name = "tmp-dir"
+              emptyDir = {}
+            }
+          ]
+        }
+      }
+    }
+  }
+}
+
 
 ################################################################################
 # Notes
