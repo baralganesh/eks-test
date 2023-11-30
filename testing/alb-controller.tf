df22e94e08a5c2de9a3ea969b1a244f325b6a4d5
G Baral <ganesh.baral@hotmail.co.uk>
2023-11-30 15:10:35 +0000

    status fixed

--- testing/alb-controller.tf
+++ testing/alb-controller.tf
@@ -636,17 +636,17 @@ resource "kubernetes_manifest" "customresourcedefinition_targetgroupbindings_elb
                   ]
                   "type" = "object"
                 }
-                #"status" = {
-                #  "description" = "TargetGroupBindingStatus defines the observed state of TargetGroupBinding"
-                #  "properties" = {
-                #    "observedGeneration" = {
-                #      "description" = "The generation observed by the TargetGroupBinding controller."
-                #      "format" = "int64"
-                #      "type" = "integer"
-                #    }
-                #  }
-                #  "type" = "object"
-                #}
+                "status" = {
+                  "description" = "TargetGroupBindingStatus defines the observed state of TargetGroupBinding"
+                  "properties" = {
+                    "observedGeneration" = {
+                      "description" = "The generation observed by the TargetGroupBinding controller."
+                      "format" = "int64"
+                      "type" = "integer"
+                    }
+                  }
+                  "type" = "object"
+                }
               }
               "type" = "object"
             }
@@ -659,14 +659,14 @@ resource "kubernetes_manifest" "customresourcedefinition_targetgroupbindings_elb
         },
       ]
     }
-    "status" = {
-      "acceptedNames" = {
-        "kind" = ""
-        "plural" = ""
-      }
-      "conditions" = []
-      "storedVersions" = []
-    }
+    #"status" = {
+    #  "acceptedNames" = {
+    #    "kind" = ""
+    #    "plural" = ""
+    #  }
+    #  "conditions" = []
+    #  "storedVersions" = []
+    #}
   }
 }
 
