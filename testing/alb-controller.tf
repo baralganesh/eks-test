615a2cbdef6e0992ed8e479d40244c90d7ebf5e5
G Baral <ganesh.baral@hotmail.co.uk>
2023-11-30 19:48:59 +0000

    helm added

---
+++ testing/alb-controller.tf
@@ -0,0 +1,13 @@
+resource "helm_release" "aws_load_balancer_controller" {
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  set {
+    name  = "clusterName"
+    value = local.cluster_name
+  }
+
+  # Specify additional configurations as needed
+}
--- testing/providers.tf
+++ testing/providers.tf
@@ -1,6 +1,5 @@
 terraform {
   required_version = ">= 0.13.1"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,20 +9,19 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "2.10"
     }
-  }
-  
-  # export TF_CLI_CONFIG_FILE="$HOME/.terraform.d/.terraformrc-leap-dev"
-  cloud {
-    organization = "gbaral-dev"
-
-    workspaces {
-      name = "BankUnited-Community-DigitalBanking-Leap-DB-dev-poc" # CREATE WORKSPACE FIRST
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.5.1" # Specify the version you want to use
     }
   }
-
+  cloud {
+    organization = "gbaral-dev"
+    workspaces {
+      name = "BankUnited-Community-DigitalBanking-Leap-DB-dev-poc"
+    }
+  }
 }
 
-# AWS credentials used is defined in TFE Env Variables
 provider "aws" {
   region = "us-east-1"
   assume_role {
@@ -31,8 +29,16 @@ provider "aws" {
   }
 }
 
-  provider "kubernetes" {
+provider "kubernetes" {
   host = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token = data.aws_eks_cluster_auth.eks.token
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.eks.token
+  }
 }
