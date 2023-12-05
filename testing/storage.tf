f4ec2f44e96719a755370204853c50691aa9654a
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-05 19:55:56 +0000

    version update

--- testing/storage.tf
+++ testing/storage.tf
@@ -2,7 +2,7 @@
 resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name      = module.eks.cluster_id
   addon_name        = "aws-ebs-csi-driver"
-  addon_version     = "v1.25.0-eksbuild.1"  # Ensure this version is compatible
+  addon_version     = "v1.23.2-eksbuild.1"  # Ensure this version is compatible
   resolve_conflicts = "OVERWRITE"
 }
 
