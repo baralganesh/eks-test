af9625572bc4d6be2e3b76b824e8796a3fb329d2
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-07 18:14:24 +0000

    addons_iam update

--- testing/storage.tf
+++ testing/storage.tf
@@ -4,6 +4,7 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   addon_name        = "aws-ebs-csi-driver"
   addon_version     = "v1.23.2-eksbuild.1"  # Ensure this version is compatible
   resolve_conflicts = "OVERWRITE"
+  service_account_role_arn = "arn:aws:iam::031342435657:role/AmazonEKS_EBS_CSI_DriverRole_tfeks_test"  # Replace with your IAM role ARN
 }
 
 # EFS CSI Driver
