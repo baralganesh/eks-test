a179309f077bc5497cec5053d6da3aff0545d4a7
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-12 14:19:18 +0000

    output update

--- testing/output.tf
+++ testing/output.tf
@@ -167,6 +167,6 @@ output "aws_auth_configmap_yaml" {
 ################################################################################
 
 output "ebs_csi_driver_role_arn" {
-  value = aws_iam_role.ebs_csi_driver.arn
+  value = module.ebs_csi_driver.arn
   description = "The ARN of the IAM role created for the EBS CSI Driver"
 }
