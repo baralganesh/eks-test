150bd2d5ac01eae71914420dcb4861d0c2a45e6f
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-12 22:16:18 +0000

    update

--- testing/storage_ebs.tf
+++ testing/storage_ebs.tf
@@ -6,16 +6,6 @@ resource "aws_eks_addon" "ebs_csi" {
   addon_name        = "aws-ebs-csi-driver"
   addon_version     = "v1.23.2-eksbuild.1" # Replace with the actual version you want to install
   resolve_conflicts = "OVERWRITE"
-  #attach_ebs_csi_policy = true
+  service_account_role_arn = "arn:aws:iam::${local.account_id}:role/${aws_iam_role.ebs_csi_driver_role.arn}" 
 }
 
-# Create Kubernetes service account for EBS CSI driver
-resource "kubernetes_service_account" "ebs_csi_sa" {
-  metadata {
-    name      = "aws-ebs-csi-driver-sa"
-    namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.ebs_csi_driver_role.arn
-    }
-  }
-}
