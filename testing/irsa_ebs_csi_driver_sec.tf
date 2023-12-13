fdd7780fe855b7d22d057262026b781536637e5a
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-13 22:29:53 +0000

    irsa changes

---
+++ testing/irsa_ebs_csi_driver_sec.tf
@@ -0,0 +1,20 @@
+# ---------------------------------------------------------------
+# service account for ebs_csi_driver
+# ---------------------------------------------------------------
+module "ebs_csi_driver_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "4.14.0"
+
+  role_name        = "${module.eks.cluster_id}-ebs-csi-driver"
+  role_description = "IRSA role for EBS CSI Driver" 
+
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
