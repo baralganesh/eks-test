3b8682bae9d1e85cf1ba4f9577f5acdbc6c0c785
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-19 14:59:26 +0000

    tet

--- testing/irsa_ebs_csi_driver_sec.tf
+++ testing/irsa_ebs_csi_driver_sec.tf
@@ -2,27 +2,25 @@
 # service account for ebs_csi_driver
 # ---------------------------------------------------------------
 
-module "ebs_csi_driver_irsa" {
+module "ebs_csi_irsa_role" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "4.14.0"
 
   role_name        = "${module.eks.cluster_id}-ebs-csi-driver"
-  role_description = "IRSA role for EBS CSI Driver"
+  role_description = "IRSA role for EBS CSI Driver" 
 
-  oidc_provider_arn = "arn:aws:iam::${local.account_id}:oidc-provider/${module.eks.oidc_provider}"
-
+  attach_ebs_csi_policy = true
+  
   oidc_providers = {
     main = {
-      provider_arn               = oidc_provider_arn
+      provider_arn               = module.eks.oidc_provider
       namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
       conditions                = {
         StringEquals = {
-          "${oidc_provider_arn}:aud": "sts.amazonaws.com",
-          "${oidc_provider_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${module.eks.oidc_provider}:aud": "sts.amazonaws.com",
+          "${module.eks.oidc_provider}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
         }
       }
     }
   }
-
-  attach_policy_arns = ["arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"]
 }
