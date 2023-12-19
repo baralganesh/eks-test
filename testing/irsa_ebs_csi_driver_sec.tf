f20c1f49eb597b646b66b2ea4a962956c26ea01a
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-19 14:20:26 +0000

    test

--- testing/irsa_ebs_csi_driver_sec.tf
+++ testing/irsa_ebs_csi_driver_sec.tf
@@ -1,6 +1,7 @@
 # ---------------------------------------------------------------
 # service account for ebs_csi_driver
 # ---------------------------------------------------------------
+
 module "ebs_csi_driver_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "4.14.0"
@@ -10,20 +11,18 @@ module "ebs_csi_driver_irsa" {
 
   attach_ebs_csi_policy = true
 
-  oidc_provider_arn = module.eks.oidc_provider
-
   oidc_providers = {
     main = {
       provider_arn               = oidc_provider_arn
       namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
       conditions                = {
         StringEquals = {
-          "${oidc_provider_arn}:aud": "sts.amazonaws.com"
+          "${module.eks.oidc_provider}:aud": "sts.amazonaws.com"
         }
       }
       conditions                = {
         StringEquals = {
-          "${oidc_provider_arn}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${omodule.eks.oidc_provider}:sub": "system:serviceaccount:kube-system:ebs-csi-controller-sa"
         }
       }
     }
