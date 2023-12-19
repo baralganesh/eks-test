43579f44cf27a99286109979a5cbf8d941431831
G Baral <ganesh.baral@hotmail.co.uk>
2023-12-19 17:38:59 +0000

    version fixed

--- testing/storage_efs.tf
+++ testing/storage_efs.tf
@@ -5,7 +5,7 @@
 resource "aws_eks_addon" "efs_csi" {
   cluster_name   = module.eks.cluster_id
   addon_name     = "aws-efs-csi-driver"
-  addon_version  = "v1.7-eksbuild.1" # Replace with the actual version you want to install
+  addon_version  = "v1.7.1-eksbuild.1" # Replace with the actual version you want to install
   resolve_conflicts = "OVERWRITE"
   service_account_role_arn = "arn:aws:iam::${local.account_id}:role/${module.eks.cluster_id}-efs-csi-driver"
 }
